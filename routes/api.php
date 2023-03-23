2a42ee5bf7156050b79a75d2a03cb97048de2973
Nathan <nathan48silva@gmail.com>
2023-03-23 13:21:12 -0300

    Support e Lessons criados

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\{CourseController, ModuleController};
+use App\Http\Controllers\Api\{
+      CourseController
+    , LessonController
+    , ModuleController
+    , SupportController};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +12,11 @@ Route::get ('/courses', [CourseController::class, 'index']);
 Route::get ('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get ('/courses/{id}/modules', [ModuleController::class, 'index']);
+
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
+
+Route::get('/supports', [SupportController::class, 'index']);
 
 Route::get('/', function () {
     return response()->json([
